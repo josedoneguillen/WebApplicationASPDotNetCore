f6156c3cdc28dabc7535aed0d1b684f7c9430322
Jose Doñe <josedoneguillen1992@hotmail.com>
2021-06-11 12:57:12 -0400

    Initializing project and getting entity framework installed via nuget packeges

---
+++ WebApplicationASPDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationASPDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
